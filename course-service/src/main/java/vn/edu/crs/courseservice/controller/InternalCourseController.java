a7288452f21b3d9abec51bc5010c40805bedadc0
leminhanh301 <leminhanh301@gmail.com>
2026-09-02 18:21:09 +0700

    fix(course-service): implement InternalCourseController and GlobalExceptionHandler for seat reservation

--- course-service/src/main/java/vn/edu/crs/courseservice/controller/InternalCourseController.java
+++ course-service/src/main/java/vn/edu/crs/courseservice/controller/InternalCourseController.java
@@ -1,4 +1,29 @@
 package vn.edu.crs.courseservice.controller;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+import vn.edu.crs.courseservice.dto.CourseDTO;
+import vn.edu.crs.courseservice.service.CourseService;
+
+@RestController
+@RequestMapping("/internal/courses")
 public class InternalCourseController {
+
+    private final CourseService courseService;
+
+    public InternalCourseController(CourseService courseService) {
+        this.courseService = courseService;
+    }
+
+    @PatchMapping("/{id}/reserve-seat")
+    @ResponseStatus(HttpStatus.OK)
+    public CourseDTO reserveSeat(@PathVariable Long id) {
+        return courseService.reserveSeat(id);
+    }
+
+    @PatchMapping("/{id}/release-seat")
+    @ResponseStatus(HttpStatus.OK)
+    public CourseDTO releaseSeat(@PathVariable Long id) {
+        return courseService.releaseSeat(id);
+    }
 }
